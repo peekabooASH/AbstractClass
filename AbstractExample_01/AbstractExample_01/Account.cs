28ae365a8ecbebb4ff2c9d5c97189112c912d340
peekabooASH <maloncho04@gmail.com>
2014-12-12 23:53:28 +0600

    first commit

---
+++ AbstractExample_01/AbstractExample_01/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractExample_01
+{
+    abstract class Account
+    {
+        public string No { set; get; }
+        public double Balance { protected set; get; }
+        public string Deposit(double amount)
+        {
+            Balance += amount;
+            return "Done";
+        }
+
+        public abstract string Withdraw(double amount);
+
+    }
+}
---
+++ AbstractExample_01/AbstractExample_01/SavingsAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractExample_01
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { set; get; }
+
+        public override string Withdraw(double amount)
+        {
+            Balance -= amount;
+            return "Withdraw";
+        }
+    }
+}
